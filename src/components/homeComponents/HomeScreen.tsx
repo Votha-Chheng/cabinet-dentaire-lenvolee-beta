cbe4d17d0f5306d52aea47810451c0098eb1fac1
Votha-Chheng <votha_chheng@gmx.fr>
2024-10-10 00:21:31 +0200

    Ajout de nouveaux onglets - Base de travail

--- src/components/homeComponents/HomeScreen.tsx
+++ src/components/homeComponents/HomeScreen.tsx
@@ -8,10 +8,12 @@ import ImageModal from '../shared/ImageModal'
 import { ModalContextProvider } from '@/context/modalContext'
 import Image from 'next/image'
 import { motion } from 'framer-motion'
+import { InView } from 'react-intersection-observer'
 
 const HomeScreen: FC = () => {
   const [presentationInView, setPresentationInView] = useState<boolean>(false)
   const [accueilInView, setAccueilInView] = useState<boolean>(false)
+  const [viewImage, setViewImg] = useState<boolean>(false)
 
   const getBgColor = (presentationInView: boolean, accueilInView: boolean)=>{
     if(presentationInView && accueilInView){
@@ -32,10 +34,11 @@ const HomeScreen: FC = () => {
         <AnchorSection/>
         <motion.div 
           initial={{opacity:0}}
-          animate={presentationInView ? {opacity:1}:{}}
+          animate={viewImage ? {opacity:1}:{}}
           transition={{duraion:0.25}}
           className='w-full h-96 relative mt-64 tablet:hidden block border-t-main-theme border-t'
         >
+          <InView triggerOnce={true} onChange={(inView, entry)=> setViewImg(inView)} />
           <Image src="/images/team.jpg" alt="Equipe complète du Cabinet dentaire L'Envolée à Montpellier/Castelnau-le-lez" className='' fill style={{objectFit:"cover"}} />
         </motion.div>
         <PresentationSection setPresentationInView={setPresentationInView}/>
