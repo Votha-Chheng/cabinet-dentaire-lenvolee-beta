8a95df7781f3532044c17671ac6dd11082614f83
Votha-Chheng <votha_chheng@gmx.fr>
2025-05-23 00:48:08 +0200

    Ajout d'une nouvelle photo portrait

--- src/components/homeComponents/Presentation/FicheTextLeft.tsx
+++ src/components/homeComponents/Presentation/FicheTextLeft.tsx
@@ -5,7 +5,7 @@ import { opacities } from '@/animations/opacityAnim'
 import LineThrough from '@/components/shared/LineThrough'
 import { frankRuhl } from '@/fonts/frankRuhl'
 import { tauri } from '@/fonts/tauri'
-import { delay, motion } from 'framer-motion'
+import { motion } from 'framer-motion'
 import Image from 'next/image'
 import React, { FC, useEffect, useMemo, useRef, useState } from 'react'
 import DiplomesSection from './DiplomesSection'
@@ -32,7 +32,13 @@ const FicheTextLeft: FC<FicheProps> = ({children, nom, fonction, inView, img, di
     <>
       <aside className='w-full h-full flex flex-col justify-between items-center text-main-theme pt-5'>
         <div className='w-full'>
-          <motion.h2 variants={appearFromBottom} custom={0.8} initial="initial" animate={inView ? "animate":""} className={`${frankRuhl.className} text-center font-semibold uppercase py-1 text-2xl laptop:text-4xl`}>
+          <motion.h2 
+            variants={appearFromBottom} 
+            custom={0.8} 
+            initial="initial" 
+            animate={inView ? "animate":""} 
+            className={`${frankRuhl.className} text-center font-semibold uppercase py-1 text-2xl laptop:text-4xl`}
+          >
             {nom}
           </motion.h2>
           <motion.h2 
@@ -50,9 +56,9 @@ const FicheTextLeft: FC<FicheProps> = ({children, nom, fonction, inView, img, di
         <InView onChange={(inView, entry)=>setDiplomeInview(inView)} triggerOnce />
         { 
           diplomes &&  
-            <motion.div variants={opacities} initial="initial" animate={`${diplomeInview ? "animate":""}`} custom={{delay:0.4, duration: 0.3}} className='w-full relative'>
-              <DiplomesSection diplomeListe={diplomeListe} setTopXDiplomeSection={setTopXDiplomeSection} height={height} />
-            </motion.div>
+          <motion.div variants={opacities} initial="initial" animate={`${diplomeInview ? "animate":""}`} custom={{delay:0.4, duration: 0.3}} className='w-full relative'>
+            <DiplomesSection diplomeListe={diplomeListe} setTopXDiplomeSection={setTopXDiplomeSection} height={height} />
+          </motion.div>
         }
       </aside>
       <LineThrough height='100%' width='1.5px' bgColor="bg-main-theme" variants={vertical} delay={0.5} animate={inView ? "animate":""} originY={1} classComplement='self-start' />
--- src/fonts/frankRuhl.ts
+++ src/fonts/frankRuhl.ts
@@ -1,5 +1,6 @@
 import { Frank_Ruhl_Libre } from "next/font/google";
 
+
 export const frankRuhl = Frank_Ruhl_Libre({
   weight: ["300", "400", "500", "600", "700", "800", "900"],
   style: ["normal"],
